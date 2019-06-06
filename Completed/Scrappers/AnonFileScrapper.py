156a2429d852aa6dd2c815572f3065175e7a6c7d
Hiddenx <demonmanoj@gmail.com>
2019-06-06 17:56:14 +0530

    June 7th - 4:39 PM
    Finished up URL - Check and Store into a Text file.
    
    -Need to write Readme.md for URLChecker
    
    Signed-off-by: Hiddenx <demonmanoj@gmail.com>

---
+++ Completed/Scrappers/AnonFileScrapper.py
@@ -0,0 +1,72 @@
+import requests
+
+writtenlinks = []
+counter = 1
+
+while True:
+    src = requests.get(
+        'https://www.bing.com/search?q=https%3a%2f%2fcdn.anonfile.com%2f+site%3ahttps%3a%2f%2fanonfile.com%2f&first=' + str(
+            counter) + '&FORM=PERE', timeout=10).text
+    links = src.split('<h2><a href="')[1:]
+    for link in links:
+        link = link.split('"')[0]
+        if link in writtenlinks:
+            continue
+        else:
+            handle = open('AnonFilelinks.txt', 'a')
+            handle.write(link + '\n')
+            print(link)
+            writtenlinks.append(link)
+    src = requests.get(
+        'https://www.bing.com/search?q=https%3a%2f%2fcdn.anonfile.com%2f+site%3ahttps%3a%2f%2fanonfile.com%2f&first=' + str(
+            counter) + '&FORM=PERE2', timeout=10).text
+    links = src.split('<h2><a href="')[1:]
+    for link in links:
+        link = link.split('"')[0]
+        if link in writtenlinks:
+            continue
+        else:
+            handle = open('AnonFilelinks.txt', 'a')
+            handle.write(link + '\n')
+            print(link)
+            writtenlinks.append(link)
+    src = requests.get(
+        'https://www.bing.com/search?q=https%3a%2f%2fcdn.anonfile.com%2f+site%3ahttps%3a%2f%2fanonfile.com%2f&first=' + str(
+            counter) + '&FORM=PERE3', timeout=10).text
+    links = src.split('<h2><a href="')[1:]
+    for link in links:
+        link = link.split('"')[0]
+        if link in writtenlinks:
+            continue
+        else:
+            handle = open('AnonFilelinks.txt', 'a')
+            handle.write(link + '\n')
+            print(link)
+            writtenlinks.append(link)
+    src = requests.get(
+        'https://www.bing.com/search?q=https%3a%2f%2fcdn.anonfile.com%2f+site%3ahttps%3a%2f%2fanonfile.com%2f&first=' + str(
+            counter) + '&FORM=PERE4', timeout=10).text
+    links = src.split('<h2><a href="')[1:]
+    for link in links:
+        link = link.split('"')[0]
+        if link in writtenlinks:
+            continue
+        else:
+            handle = open('AnonFilelinks.txt', 'a')
+            handle.write(link + '\n')
+            print(link)
+            writtenlinks.append(link)
+    src = requests.get(
+        'https://www.bing.com/search?q=https%3a%2f%2fcdn.anonfile.com%2f+site%3ahttps%3a%2f%2fanonfile.com%2f&first=' + str(
+            counter) + '&FORM=PERE5', timeout=10).text
+    links = src.split('<h2><a href="')[1:]
+    for link in links:
+        link = link.split('"')[0]
+        if link in writtenlinks:
+            continue
+        else:
+            handle = open('AnonFilelinks.txt', 'a')
+            handle.write(link + '\n')
+            print(link)
+            writtenlinks.append(link)
+    counter += 10
