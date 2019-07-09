ace5668ab7f11580957259cb63ff274e444fb866
hiddenxx <demonmanoj@gmail.com>
2019-07-09 00:58:27 -0700

    Changed

--- Shell_Scripts/git-create.sh
+++ Shell_Scripts/git-create.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 repo_name=$1
+
 github_username=hiddenxx
 
-if [[ ! -e $repo_name ]]; then
-    mkdir $repo_name
-elif [[ ! -d $repo_name ]]; then
-    echo "$repo_name already exists but is not a directory" 1>&2
-fi
+mkdir $repo_name
 
 cd $repo_name
+
 git init
+
 git add . && git commit -m "First Commit"
 
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
